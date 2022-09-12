da2b2f8eddcdb3706b574e8e73fa8136d177d1d3
osaja <osaja@DELLVOSTRO>
2022-09-12 20:02:13 +0200

    Add project files.

---
+++ ScaryClown/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaryClown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IClown fingersTheClown = new ScaryScary("big red nose", 14);
+
+            fingersTheClown.Honk();
+
+            //IScaryClown iScaryClownReference = fingersTheClown;
+            //iScaryClownReference.ScareLittleChildren();
+
+            if (fingersTheClown is ScaryScary scaryClown)
+                scaryClown.ScareLittleChildren();
+        }
+    }
+}
